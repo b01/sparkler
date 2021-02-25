61effe5c8e17a329708d5839a1bbf17da4b92508
Khalifah Khalil Shabazz <shabazzk@gmail.com>
2021-02-24 23:51:47 -0500

    Added Spark Design System builder.

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// import the Spark JavaScript
+import spark from "@sparkdesignsystem/spark/es5/spark";
+import sparkPrerender from "@sparkdesignsystem/spark/es5/sparkPrerender";
+// optional (see below)
+import "@sparkdesignsystem/spark/es5/sparkPolyfills";
+// import the Spark CSS
+import "@sparkdesignsystem/spark-styles/_spark.scss";
+// // initialize Spark
+sparkPrerender();
+spark();
+
+function component() {
+    const element = document.createElement('div');
+    element.innerHTML = 'Hello, Spark! JavaScript is working!';
+    return element;
+}
+document.body.appendChild(component());
